0c4898a31987ac9b8b4c304917bcf6c2f8df01e4
Vitaly Golubev <vitalyg1988@gmail.com>
2019-01-28 14:04:28 +0400

    create main-file

---
+++ main.py
@@ -0,0 +1,10 @@
+class Animals():
+    satiety = True
+
+    def __init__(self, name, paws, weight, color):
+        self.name = name
+        self.paws = paws
+        self.weight = weight
+        self.color = color
+
+    def
